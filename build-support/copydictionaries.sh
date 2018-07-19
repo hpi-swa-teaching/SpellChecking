409a50576bdb46cff6b9e3d11405ccaccd8d5c08
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-19 21:07:46 +0200

    added an echo statement to make it easier to debug

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -1,3 +1,4 @@
 cp -R -v $PROJECT_HOME/build-support/dictionaries ${BUILD_PATH}/
 ls -la $PROJECT_HOME/dictionaries
 chmod -R 777 ${BUILD_PATH}/dictionaries/*
+echo 'loaded dictionaries for hunspell'
