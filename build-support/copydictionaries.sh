5cd625d1c9e9e7ff43e55763ed705ec4341439c8
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-19 21:24:03 +0200

    [WIP] more output

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -1,4 +1,7 @@
+echo $PROJECT_HOME
+echo ${BUILD_PATH}
 cp -R -v $PROJECT_HOME/build-support/dictionaries ${BUILD_PATH}/
-ls -la $PROJECT_HOME/dictionaries
+echo ${BUILD_PATH}/dictionaries
+ls -la ${BUILD_PATH}/dictionaries
 chmod -R 777 ${BUILD_PATH}/dictionaries/*
 echo 'loaded dictionaries for hunspell'
