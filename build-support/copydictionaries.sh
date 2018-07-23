bf2d318059b14e49b7ce5976892a3a8725b300ca
Fabio Niephaus <code@fniephaus.com>
2018-07-23 14:26:25 +0200

    Minor cleanup

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -v
+set -e
 mkdir -p ${SMALLTALK_CI_BUILD}
 cp -R ${TRAVIS_BUILD_DIR}/build-support/dictionaries ${SMALLTALK_CI_BUILD}/
 echo 'loaded dictionaries for hunspell'
