5d3b903a2cd8da5b5c0437d47c1c04f6cbe93ddf
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-23 15:13:05 +0200

    added install script for hunspell

---
+++ build-support/installHunspellOnLinux.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+sudo apt-get install libhunspell-1.3-0:i386
+ln -s /usr/lib/i386-linux-gnu/libhunspell-1.3.so.0 libhunspell-1.3.so.0
+echo 'loaded dictionaries for hunspell'
