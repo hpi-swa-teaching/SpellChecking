f5b104eaa6cfa7cf125f81dd143ae4d480c2079a
Fabio Niephaus <code@fniephaus.com>
2018-07-23 14:22:05 +0200

    Update copydictionaries.sh

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -1,19 +1,5 @@
-echo $TRAVIS_OS_NAME
-
-IMAGE_PATH="/home/travis/smalltalkCI-master/_builds/Roboto/"
-
-if [ "$TRAVIS_OS_NAME" == "osx" ];
-  then IMAGE_PATH="/Users/travis/smalltalkCI-master/_builds/Roboto/";
-fi;
-
-mkdir -p $IMAGE_PATH
-cp -R $TRAVIS_BUILD_DIR/build-support/dictionaries $IMAGE_PATH
-chmod -R 777 $IMAGE_PATH
-
-ls -lisa $IMAGE_PATH
-
-
-echo $TRAVIS_BUILD_DIR
-cp -R -v $TRAVIS_BUILD_DIR/build-support/dictionaries ${BUILD_PATH}/
-chmod -R 777 ${BUILD_PATH}/dictionaries/*
+#!/usr/bin/env bash
+set -v
+mkdir -p ${SMALLTALK_CI_BUILD}
+cp -R ${TRAVIS_BUILD_DIR}/build-support/dictionaries ${SMALLTALK_CI_BUILD}/
 echo 'loaded dictionaries for hunspell'
