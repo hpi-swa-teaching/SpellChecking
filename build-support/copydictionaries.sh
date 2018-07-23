5a9d6efd04fb0c82e358387e94b43f6918145568
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-23 13:34:58 +0200

    Update copydictionaries.sh

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -14,9 +14,6 @@ ls -lisa $IMAGE_PATH
 
 
 echo $TRAVIS_BUILD_DIR
-echo ${BUILD_PATH}
-echo $SMALLTALK_CI_BUILD
 cp -R -v $TRAVIS_BUILD_DIR/build-support/dictionaries ${BUILD_PATH}/
-ls -la $TRAVIS_BUILD_DIR
 chmod -R 777 ${BUILD_PATH}/dictionaries/*
 echo 'loaded dictionaries for hunspell'
