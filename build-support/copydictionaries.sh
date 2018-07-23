e9177075ed92558797a3c6a4a2ecb2aa1ad4934c
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-23 13:30:45 +0200

    Update copydictionaries.sh

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -1,3 +1,18 @@
+echo $TRAVIS_OS_NAME
+
+IMAGE_PATH="/home/travis/smalltalkCI-master/_builds/Roboto/"
+
+if [ "$TRAVIS_OS_NAME" == "osx" ];
+  then IMAGE_PATH="/Users/travis/smalltalkCI-master/_builds/Roboto/";
+fi;
+
+mkdir -p $IMAGE_PATH
+cp -R $TRAVIS_BUILD_DIR/build-support/dictionaries $IMAGE_PATH
+chmod -R 777 $IMAGE_PATH
+
+ls -lisa $IMAGE_PATH
+
+
 echo $TRAVIS_BUILD_DIR
 echo ${BUILD_PATH}
 echo $SMALLTALK_CI_BUILD
