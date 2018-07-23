7786a94b1093cd7277eff8b4923f83014fe3ccbd
Robert Schwanhold <r.schwanhold@hotmail.de>
2018-07-23 13:13:34 +0200

    Update copydictionaries.sh

--- build-support/copydictionaries.sh
+++ build-support/copydictionaries.sh
@@ -1,6 +1,7 @@
-echo $PROJECT_HOME
+echo $TRAVIS_BUILD_DIR
 echo ${BUILD_PATH}
-cp -R -v $PROJECT_HOME/build-support/dictionaries ${BUILD_PATH}/
+echo $SMALLTALK_CI_BUILD
+cp -R -v $TRAVIS_BUILD_DIR/build-support/dictionaries ${BUILD_PATH}/
 echo ${BUILD_PATH}/dictionaries
 ls -la ${BUILD_PATH}/dictionaries
 chmod -R 777 ${BUILD_PATH}/dictionaries/*
