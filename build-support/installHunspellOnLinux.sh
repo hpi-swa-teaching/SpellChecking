9b54dcc4800a30407518eb85ac96ab7ac06cc438
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-17 19:09:28 +0200

    New version of stuff.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -v
-sudo apt-get install libhunspell-1.3-0:i386
-ln -s /usr/lib/i386-linux-gnu/libhunspell-1.3.so.0 libhunspell-1.3.so.0
+sudo apt-get install libhunspell-1.6-0
+ln -s /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0 libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
