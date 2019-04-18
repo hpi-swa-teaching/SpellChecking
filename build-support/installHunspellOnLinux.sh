d10f56282a6b8b613a9fbcd17b88e93064ee904e
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-18 14:38:18 +0200

    Investigating Travis build.7

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -v
 sudo apt-get install libhunspell-1.6-0
-ln -s /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0 libhunspell-1.6.so.0
+ln -s /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0 libhunspell-1.3.so.0
+pwd
 echo 'loaded dictionaries for hunspell'
