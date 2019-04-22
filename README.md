1579b7c3038f74b294f1a45b853d575f574c4070
Eric Ackermann <eric.ackermann.99@googlemail.com>
2019-04-22 20:04:14 +0200

    Same location for install scripts.

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ your Git repository to, e.g.
 Make sure there is no slash at the end of the path.
 
 ### Windows
-Copy *install-dependencies.cmd* to the root directory of your image (same folder as *Contents*) and run it in a cmd window   
+Copy *install-dependencies.cmd* to the root directory of your image (same folder as *squeak.bat*) and run it in a cmd window   
 > install-dependencies.cmd <path to the root of your cloned Git Repo> <br/>
 
 Start Squeak, and run the following code in a workspace:
--- install-dependencies.cmd
+++ install-dependencies.cmd
@@ -5,7 +5,7 @@ set dicts=\build-support\dictionaries
 set sourceHunspell32=%gitrepo%%filename32%
 set sourceHunspell64=%gitrepo%%filename64%
 set dictPath=%gitrepo%%dicts%
-copy /B %sourceHunspell32% .\Contents\Win32\
-copy /B %sourceHunspell64% .\Contents\Win32\
-mkdir .\Contents\Resources\dictionaries
-xcopy /S %dictPath% .\Contents\Resources\dictionaries
+copy /B %sourceHunspell32% .\SWT2019Trunk.app\Contents\Win32\
+copy /B %sourceHunspell64% .\SWT2019Trunk.app\Contents\Win32\
+mkdir .\SWT2019Trunk.app\Contents\Resources\dictionaries
+xcopy /S %dictPath% .\SWT2019Trunk.app\Contents\Resources\dictionaries
