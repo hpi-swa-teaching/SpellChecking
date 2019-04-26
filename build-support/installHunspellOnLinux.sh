dbfa6becafb3bac7f487bd03c49244e1aec7e652
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 18:17:10 +0200

    Trying to copy wrong file to correct file.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -v
 sudo apt-get install libhunspell-1.3-0
-sudo ln -s /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
-sudo ls -la /usr/lib/x86_64-linux-gnu
+sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
