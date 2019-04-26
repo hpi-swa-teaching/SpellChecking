50022e6be8a33d1834f8d6561d39c83ae67bab64
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 17:26:54 +0200

    Manually copying binary.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -v
-sudo apt-get install libhunspell-1.3-0:i386
-ln -s /usr/lib/i386-linux-gnu/libhunspell-1.3.so.0 /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0
-pwd
+sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
