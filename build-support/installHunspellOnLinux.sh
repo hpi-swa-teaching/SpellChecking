4696d9798f3972b8274470445532a21680351e00
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 17:49:12 +0200

    Trying ubuntu bionic (again).

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -v
-sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /lib/x86_64-linux-gnu/libhunspell-1.6.so.0
-sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /lib/i386-linux-gnu/libhunspell-1.6.so.0
+sudo apt install libhunspell-1.6-0
 sudo ls -la /lib/i386-linux-gnu
 echo 'loaded dictionaries for hunspell'
