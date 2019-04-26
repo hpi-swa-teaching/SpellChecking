7467accd912da4a6facf177fb281e5152d81269d
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 18:43:21 +0200

    Copy directive.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -2,6 +2,7 @@
 set -v
 sudo apt-get install libhunspell-1.3-0:i386
 sudo dpkg -L libhunspell-1.3-0
-sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
-sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0 /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0
+file /usr/lib/i386-linux-gnu/libhunspell-1.3.so.0.0.0
+sudo cp /usr/lib/i386-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo cp /usr/lib/i386-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
