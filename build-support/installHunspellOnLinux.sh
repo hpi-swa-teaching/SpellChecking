c4828899966a208144daf337ecf767250936be14
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 17:38:09 +0200

    Trying different folder for library.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -v
-sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /lib/i386-linux-gnu/libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
