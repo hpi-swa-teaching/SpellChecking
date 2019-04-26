7349035aa0262aa3423b83eb454fbe17f10fa20b
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 18:12:14 +0200

    Creating a symbolic link.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -v
 sudo apt-get install libhunspell-1.3-0
-dpkg -L libhunspell-1.3-0
+sudo ln -s /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo ls -la /usr/lib/x86_64-linux-gnu
 echo 'loaded dictionaries for hunspell'
