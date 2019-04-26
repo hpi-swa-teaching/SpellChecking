d99652b20422792a8394b4a3df6390b37bcf1bf3
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 18:40:18 +0200

    Finding package again.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -v
 sudo apt-get install libhunspell-1.3-0:i386
-sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
-sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0
+sudo dpkg -L libhunspell-1.3-0
+sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0 /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
