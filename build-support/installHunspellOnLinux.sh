2561a52e954cbc027f52e35a9503ab172c04ef47
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 17:42:08 +0200

    Debugging file not found.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -2,4 +2,5 @@
 set -v
 sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /lib/x86_64-linux-gnu/libhunspell-1.6.so.0
 sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.3.so.0 /lib/i386-linux-gnu/libhunspell-1.6.so.0
+sudo ls -la /lib/i386-linux-gnu
 echo 'loaded dictionaries for hunspell'
