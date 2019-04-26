5e44886432c267a6a3f128b6e9a404305960993b
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-04-26 18:37:39 +0200

    i386 again.

--- build-support/installHunspellOnLinux.sh
+++ build-support/installHunspellOnLinux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -v
-sudo apt search libhunspell
-sudo cp $TRAVIS_BUILD_DIR/libhunspell-1.6.so.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo apt-get install libhunspell-1.3-0:i386
+sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/x86_64-linux-gnu/libhunspell-1.6.so.0
+sudo cp /usr/lib/x86_64-linux-gnu/libhunspell-1.3.so.0.0.0 /usr/lib/i386-linux-gnu/libhunspell-1.6.so.0
 echo 'loaded dictionaries for hunspell'
