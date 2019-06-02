b85429ed6f3ddba57410dd3d63c7018131cb9669
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-06-02 20:01:57 +0200

    Hook should be fixed.

--- hook.sh
+++ hook.sh
@@ -3,7 +3,7 @@ ls
 pwd
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo Current Branch: ${BRANCH}
-sed -i "s/branch=\w*/branch=${BRANCH}/g" README.md
-sed -i "s/SpellChecking:\w*/SpellChecking:${BRANCH}/g" README.md
+sed -i -r 's/branch=(\w|-)*/branch='"${BRANCH}"'/g' README.md
+sed -i -r 's/SpellChecking:(\w|-)*/SpellChecking:'"${BRANCH}"'/g' README.md
 git add README.md && git commit -m"Updated branch refs in README."
 exit 0
