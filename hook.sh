3ae62c90a082fd66c26767e567c91de97903d70d
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-06-04 18:06:18 +0200

    Added note.

--- hook.sh
+++ hook.sh
@@ -6,4 +6,5 @@ echo Current Branch: ${BRANCH}
 sed -i -r 's/branch=(\w|-)*/branch='"${BRANCH}"'/g' README.md
 sed -i -r 's/SpellChecking:(\w|-)*/SpellChecking:'"${BRANCH}"'/g' README.md
 git add README.md && git commit -m"Updated branch refs in README."
+echo Please push again.
 exit 0
